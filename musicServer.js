2c2abc22be0f209f3dc68134fab4d420326b8dc7
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-03-08 16:50:25 -0600

    Added next error handling

--- musicServer.js
+++ musicServer.js
@@ -28,3 +28,14 @@ app.get('/api/artists', (req, res, next) => {
         res.json(rows).send(rows);
     });
 });
+
+app.use((error, req, res, next) => {
+    console.error(error);
+    // res.render('errorPage'); // renders an error page 
+    res.status(error.status || 500).send({
+        error: {
+            status: error.status || 500,
+            message: error.message || "Not found, my guy!",
+        }
+    });
+});

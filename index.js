11c112b335e6617b4768ad5f19d06ec975a7cc64
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-03-08 16:48:31 -0600

    Added some file dependencies and structure

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://deezerdevs-deezer.p.rapidapi.com/album/%7Bid%7D",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "bee8030221msh246af6cee12d499p1da561jsn06a1e5d81d0e",
-		"X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
-	}
+const fetch = require('node-fetch');
+
+const url = 'https://genius-song-lyrics1.p.rapidapi.com/search/?q=weeknd&per_page=1&page=1&text_format=dom';
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'bee8030221msh246af6cee12d499p1da561jsn06a1e5d81d0e',
+    'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+  }
 };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+fetch(url, options)
+	.then(res => res.json())
+	.then(json => console.log(json))
+	.catch(err => console.error('error:' + err));
---
+++ musicServer.js
@@ -0,0 +1,30 @@
+// set up dependencies
+import { config } from 'dotenv';
+config();
+import cors from 'cors';
+import express, { json } from 'express';
+const app = express();
+// const colors = require('colors/safe');
+import { getPool } from './dbConn';
+const pool = getPool();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS'); // include OPTIONS method
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200); // respond OK to OPTIONS requests
+    }
+    next(); 
+});
+
+app.get('/api/artists', (req, res, next) => {
+    pool.query("SELECT * FROM artists", (err, res) => {
+        if (err) {
+            return next(err);
+        }
+        const rows = result.rows;
+        // console.log(rows);
+        res.json(rows).send(rows);
+    });
+});
